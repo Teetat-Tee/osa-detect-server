c9a46429db0c11fa6324c5d6def1b328c0132000
Teetat-Sunawat <106581623+Teetat-Tee@users.noreply.github.com>
2026-07-02 22:15:19 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -247,3 +247,6 @@ if __name__ == '__main__':
     load_model()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
+else:
+    # gunicorn รัน — โหลด model ตอน import
+    load_model()
